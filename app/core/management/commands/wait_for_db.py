51761ed6027b8bf993249859f67a97aa78048ad3
eduardovs <eduardo.susan@gmail.com>
2022-06-18 17:56:41 -0600

    fixed several typos

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -8,7 +8,7 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    """Django command to wait for database to be ready"""
+    """Django command to wait for database."""
 
     def handle(self, *args, **options):
         """Entrypoint for command."""
@@ -19,7 +19,7 @@ class Command(BaseCommand):
                 self.check(databases=['default'])
                 db_up = True
             except (Psycopg2OpError, OperationalError):
-                self.stdout.write('Database unavailable, waiting 1 second')
+                self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS('Database available!'))
