f5ee8551b1fd3fbcd3f9066d19ea60f6576916af
eduardovs <eduardo.susan@gmail.com>
2022-07-04 06:51:28 -0600

    created tests and api end-point for the recipe app

---
+++ app/recipe/urls.py
@@ -0,0 +1,21 @@
+"""
+URL mappings for the recipe app.
+"""
+from django.urls import (
+    path,
+    include,
+)
+
+from rest_framework.routers import DefaultRouter
+
+from recipe import views
+
+router = DefaultRouter()
+router.register('recipes', views.RecipeViewSet)
+
+# learning: name used when performing reverse lookup of urls
+app_name = 'recipe'
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
