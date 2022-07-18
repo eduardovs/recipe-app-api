7c5c2ed2496ef38ba6dfdabd2089954f4e103d20
eduardovs <eduardo.susan@gmail.com>
2022-07-18 07:14:21 -0600

    finished implementing ingredients feature. Tests are passing

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -11,7 +11,7 @@ from core.models import (
 
 
 class IngredientSerializer(serializers.ModelSerializer):
-    """Serializers for ingredients."""
+    """Serializer for ingredients."""
 
     class Meta:
         model = Ingredient
@@ -38,7 +38,7 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'title', 'time_minutes', 'price', 'link', 'tags',
             'ingredients',
-            ]
+        ]
         read_only_fields = ['id']
 
 
@@ -71,20 +71,22 @@ class RecipeDetailSerializer(RecipeSerializer):
     def create(self, validated_data):
         """Create a recipe."""
         tags = validated_data.pop('tags', [])
+        ingredients = validated_data.pop('ingredients', [])
         recipe = Recipe.objects.create(**validated_data)
         self._get_or_create_tags(tags, recipe)
+        self._get_or_create_ingredients(ingredients, recipe)
 
         return recipe
 
     def update(self, instance, validated_data):
         """Update recipe."""
         tags = validated_data.pop('tags', None)
-        ingredients = validated_data.pop('tags', None)
+        ingredients = validated_data.pop('ingredients', None)
         if tags is not None:
             instance.tags.clear()
             self._get_or_create_tags(tags, instance)
 
-        if ingredients is None:
+        if ingredients is not None:
             instance.ingredients.clear()
             self._get_or_create_ingredients(ingredients, instance)
 
